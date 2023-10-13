34c461e5678a5a4a30d8e4ec02ee12a60fa350dc
aungkoko <akk1996.htt@gmail.com>
2023-10-13 22:48:01 +0630

    refactored hello world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Aung Ko")
-	want := "Hello, Aung Ko"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Aung Ko", "")
+		want := "Hello, Aung Ko"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("call hello with empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
